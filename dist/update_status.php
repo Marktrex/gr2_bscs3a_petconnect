76eb44df867bccf3c99efba97cc66528602b828c
marktrex <sinicchi123@gmail.com>
2023-10-27 22:42:00 +0800

    81.1% working

--- dist/update_status.php
+++ dist/update_status.php
@@ -15,27 +15,37 @@ if (isset($_POST['appointmentId']) && isset($_POST['status'])) {
         $message = "Good Day, Ma'am/Sir,\n\nWe're sincerely sorry to cancel your appointment because of the sudden circumstances in our shelter. We hope for your consideration. Thank you.\n\nVery truly yours,\nRePaw City";
     }
 
-    // Escape the message to prevent SQL injection
-    $escapedMessage = mysqli_real_escape_string($conn, $message);
+    // Create a PDO instance
+    try {
+        $conn = new PDO("mysql:host=$dsn;dbname=petconnect", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+        exit;
+    }
 
-    // Update the status and message in the database
-    $query = "UPDATE appointment SET status = '$status', message = '$escapedMessage' WHERE appointment_id = '$appointmentId'";
-    $result = mysqli_query($conn, $query);
+    // Prepare and execute the SQL statement with placeholders to update the status and message
+    $query = "UPDATE appointment SET status = :status, message = :message WHERE appointment_id = :appointmentId";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':status', $status, PDO::PARAM_STR);
+    $stmt->bindParam(':message', $message, PDO::PARAM_STR);
+    $stmt->bindParam(':appointmentId', $appointmentId, PDO::PARAM_INT);
 
-    // Check if the update was successful
-    if ($result) {
-        
+    // Execute the query
+    $success = $stmt->execute();
+
+    if ($success) {
         // Return a success response
         http_response_code(200);
         echo "Status updated successfully";
     } else {
         // Return an error response
         http_response_code(500);
-        echo "Error updating status: " . mysqli_error($conn);
+        echo "Error updating status: " . $stmt->errorInfo()[2];
     }
 
     // Close the database connection
-    mysqli_close($conn);
+    $conn = null;
 } else {
     // Return an error response if the appointment ID and status values are not provided
     http_response_code(400);
