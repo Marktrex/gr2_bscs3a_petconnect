d0056c1581ffa6efca0f351e955699832547c7b6
Aries <ajtagle12@gmail.com>
2023-10-30 12:33:12 +0800

    finished positioning in login

--- dist/loginpage.php
+++ dist/loginpage.php
@@ -7,41 +7,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PetConnect</title>
-    <link rel="stylesheet" href="css/loginpage.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Acme">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sigmar">
+
+    <link rel="stylesheet" href="css/tailwind-compiling-css/output.css">
+
     <script src="https://kit.fontawesome.com/98b545cfa6.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 </head>
 
-<body>
-    <div class="login">
-        <div class="slideshow-container">
-            <img src=".\image\LoginSignup\bg1.jpg" alt="Image 1">
-            <img src=".\image\LoginSignup\bg2.jpg" alt="Image 2">
-            <img src=".\image\LoginSignup\bg3.jpg" alt="Image 3">
+<body class="tw-flex tw-items-center tw-justify-center
+            tw-h-screen tw-w-screen
+            tw-p-12">
+    <div class="tw-flex
+                tw-h-3/4 tw-w-3/4
+                custom-middle-background-color">
+        <div class="tw-hidden md:tw-flex tw-items-center tw-justify-center tw-flex-1">
+
+            <img src="./image/LoginSignup/photo-deco-1.png" alt="design photo" 
+            class= "tw-h-4/5">
         </div>
-        <a href="home.php"><img src="./image/logo (1).png" class="logo"></a>
-
-
-        <form class="login-form" name="login" action="./function/authcode.php" method="post">
-            <div class="content">
-                <h1>WELCOME!</h1>
-                <p>Let's get started</p>
-            </div>
-            <br>
+        <form class="tw-flex tw-flex-col tw-justify-center tw-flex-1"
+         name="login" action="./function/authcode.php" method="post">
             <div class="input-container">
-                <i class="fa-regular fa-user icon"></i>
                 <input class="input-field" type="email" placeholder="Email" name="email" id="username" required >
             </div>
             <div class="input-container password">
-                <i class="fa-solid fa-lock icon"></i>
                 <input class="input-field" type="password" placeholder="Password" name="password" id="password"
                     required>
                 <i class="fa-solid fa-eye" id="show-password"></i>
             </div>
             <a href="" class="forgot-pass">
-                <p></p>
+                <p>Forgot Password?</p>
             </a>
             <input type="submit" name="login" value="Login" class="loginbtn">
             <a href="signuppage.php" class="account">
@@ -51,28 +46,6 @@
 
     </div>
     <script src="./script/script.js"></script>
-
-    <script>
-        var slideIndex = 0;
-        var slides = document.getElementsByClassName("slideshow-container")[0].getElementsByTagName("img");
-
-        function showSlides() {
-            for (var i = 0; i < slides.length; i++) {
-                slides[i].style.opacity = 0;
-            }
-
-            slideIndex++;
-            if (slideIndex > slides.length) {
-                slideIndex = 1;
-            }
-
-            slides[slideIndex - 1].style.opacity = 1;
-
-            setTimeout(showSlides, 4000); // Delay between slides (2 seconds)
-        }
-
-        showSlides();
-    </script>
 </body>
 
 </html>
