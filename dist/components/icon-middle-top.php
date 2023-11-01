7d730bb6ded8dc23080e942575cf38548e71b7ad
Aries <ajtagle12@gmail.com>
2023-11-01 15:10:03 +0800

    added the logo with navbar login/sign up

--- dist/components/icon-middle-top.php
+++ dist/components/icon-middle-top.php
@@ -1,4 +1,4 @@
-<div class = "absolute right-1/2 -top-10 translate-x-1/2
-            custom-background-color rounded-full p-6 h-24 w-24 ">
-    <img src="image/icon.png" alt="icon" class="absolute top-[5px] left-[5px]">
+<!-- not used currently -->
+<div class = "w-full absolute top-0 ">
+        <img src="image/icon.png" alt="icon" class="w-100px">
 </div>
--- dist/signuppage.php
+++ dist/signuppage.php
@@ -19,67 +19,73 @@ session_start();
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 </head>
 
-<body class="flex items-center justify-center
-            h-screen w-screen
+<body class="h-screen w-screen
             custom-background-color">
 
-    <div class="flex gap-12
-        min-h-fit w-full
-        md:w-3/4
-        py-16 px-20
-        custom-frame
-        rounded-none md:rounded-[48px]">
-        
-        <div class="hidden lg:flex items-center justify-center flex-1">
-            <!-- photo for the right(change here) -->
-            <img src="./image/LoginSignup/photo-deco-2.png" alt="design photo" 
-            class= "">
-        </div>
-
-
-        <form class="flex flex-col justify-center flex-1 gap-7"
-         name="signup"  action="./function/authcode.php" method="post" autocomplete="off">
-            <div class="custom-text">
-                <h2 class = "text-5xl font-bold">Register</h2>
-                <p class= "my-3">Let's Get Started!
-                                Create an account to login.</p>
+    <div class = "h-[5rem] flex justify-start items-center ps-8 custom-navbar-color">
+        <a href="#">
+            <img src="image/icon.png" alt="icon" class="w-[75px]">
+        </a>
+    </div>
+    <div class = "flex items-center justify-center mt-10">
+        <div class="flex gap-12
+            min-h-fit w-full
+            md:w-3/4
+            py-16 px-20
+            custom-frame
+            rounded-none md:rounded-[48px]">
+            
+            <div class="hidden lg:flex items-center justify-center flex-1">
+                <!-- photo for the right(change here) -->
+                <img src="./image/LoginSignup/photo-deco-2.png" alt="design photo" 
+                class= "">
             </div>
 
-            <div class="relative">
-                <i class="fa-regular fa-user icon custom-important-text
-                        absolute top-[3px] "></i>
-                <input class="custom-input" type="text" placeholder="First Name" name="fname" id="fname" required>
-            </div>
-            <div class="mt-3 relative">
-                <i class="fa-regular fa-user icon custom-important-text
-                        absolute top-[3px] "></i>
-                <input class="custom-input" type="text" placeholder="Last Name" name="lname" id="lname" required>
-            </div>
-            <div class="mt-3 relative">
-                <i class="fa-solid fa-envelope icon custom-important-text
-                        absolute top-[3px] "></i>
-                <input class="custom-input" type="email" placeholder="Email" name="email" id="email" required>
-            </div>
-            <div class="mt-3 relative">
-                <i class="fa-solid fa-lock icon custom-important-text
-                        absolute top-[3px] "></i>
-                <input class="custom-input" type="text" placeholder="Password" name="password" id="password" required>
-            </div>
-            <div class="mt-3 relative">
-                <i class="fa-solid fa-lock icon custom-important-text
-                        absolute top-[3px] "></i>
-                <input class="custom-input" type="text" placeholder="Confirm Password" name="cpassword" id="password" required>
-            </div>
 
-            <input type="submit" name="register" value="Sign Up" class="custom-button">
-            <section class="custom-text">
-                <div class="sign-up-container">
-                    <p>Already have an Account? <a href="loginpage.php">
-                        <span class="custom-important-text hover:underline underline-offset-2">Log in </span></a></p>
+            <form class="flex flex-col justify-center flex-1 gap-7"
+            name="signup"  action="./function/authcode.php" method="post" autocomplete="off">
+                <div class="custom-text">
+                    <h2 class = "text-5xl font-bold">Register</h2>
+                    <p class= "my-3">Let's Get Started!
+                                    Create an account to login.</p>
                 </div>
-            </section>
-        </form>
 
+                <div class="relative">
+                    <i class="fa-regular fa-user icon custom-important-text
+                            absolute top-[3px] "></i>
+                    <input class="custom-input" type="text" placeholder="First Name" name="fname" id="fname" required>
+                </div>
+                <div class="mt-3 relative">
+                    <i class="fa-regular fa-user icon custom-important-text
+                            absolute top-[3px] "></i>
+                    <input class="custom-input" type="text" placeholder="Last Name" name="lname" id="lname" required>
+                </div>
+                <div class="mt-3 relative">
+                    <i class="fa-solid fa-envelope icon custom-important-text
+                            absolute top-[3px] "></i>
+                    <input class="custom-input" type="email" placeholder="Email" name="email" id="email" required>
+                </div>
+                <div class="mt-3 relative">
+                    <i class="fa-solid fa-lock icon custom-important-text
+                            absolute top-[3px] "></i>
+                    <input class="custom-input" type="text" placeholder="Password" name="password" id="password" required>
+                </div>
+                <div class="mt-3 relative">
+                    <i class="fa-solid fa-lock icon custom-important-text
+                            absolute top-[3px] "></i>
+                    <input class="custom-input" type="text" placeholder="Confirm Password" name="cpassword" id="password" required>
+                </div>
+
+                <input type="submit" name="register" value="Sign Up" class="custom-button">
+                <section class="custom-text">
+                    <div class="sign-up-container">
+                        <p>Already have an Account? <a href="loginpage.php">
+                            <span class="custom-important-text hover:underline underline-offset-2">Log in </span></a></p>
+                    </div>
+                </section>
+            </form>
+
+        </div>
     </div>
 
     <?php require_once "components/light-switch.php"?>
