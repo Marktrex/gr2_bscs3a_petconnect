2cdf3698a699b30822dfbac8bfa7bf6530bb0c64
Aries <ajtagle12@gmail.com>
2023-11-19 22:04:18 +0800

    added icon

--- dist/admin/admin-audit-trail.php
+++ dist/admin/admin-audit-trail.php
@@ -17,8 +17,8 @@ $auditLogs = $audit->getAuditLog();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<link rel="icon" href="../image/icon.png" type="image/png">
+    <meta charset="UTF-8">
+    <link rel="icon" href="../image/icon.png" type="image/png">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
