c8bf4c37b7f3a7d46fd4e58411ac67709e7a95a6
marktrex <sinicchi123@gmail.com>
2023-12-10 16:07:10 +0800

    changed the design of the forgotpassword php

--- forgotpassword.php
+++ forgotpassword.php
@@ -1,12 +1,13 @@
 <?php 
 session_start(); 
+print_r($_SESSION);
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require 'vendor/autoload.php';
 require 'dist/function/config.php';
-print_r($_SESSION);
+
 
 
 
@@ -94,69 +95,30 @@ print_r($_SESSION);
 ?>
 
 
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
-
-    <link rel="stylesheet" href="style.css">
-
-    <link rel="icon" href="Favicon.png">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-
-    <title>PetConnect | Forgot Password</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forget Password</title>
+    <link rel="stylesheet" href="dist\css\newlyAdded\forget-password.css">
 </head>
 <body>
-
-<nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
     <div class="container">
-        <a class="navbar-brand" href="#">User Password Recover</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-    </div>
-</nav>
-
-<main class="login-form">
-    <div class="cotainer">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                <div class="card-header">Enter Your Email</div>
-                    <div class="card-body">
-                        <form action="#" method="POST" name="forgotpassword">
-                            <div class="form-group row">
-                                <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
-                                <div class="col-md-6">
-                                    <input type="text" id="email_address" class="form-control" name="email" required autofocus>
-                                    <input type="submit" value="Recover" name="recover">
-                                </div>
-                            </div>
-                                                     
-                    </div>
-                    </form>
-                </div>
-            </div>
+        <h1>Forgot password?</h1>
+        <p>
+            You can reset your password by entering the email linked to <br>your account, and we'll send you an email with instructions. 
+        </p>
+        <form action="#" method="POST" >
+        <div class="code-container">
+            <input id="enter-email" type="email" name="email" placeholder="Enter your Email" required>
         </div>
+
+        <div>
+        <button type="submit" class="btn btn-verify" name="recover">Send Instructions</button>
+        </div>
+        </form>
     </div>
-    </div>
-    
-</main>
 </body>
+
 </html>
-
-
