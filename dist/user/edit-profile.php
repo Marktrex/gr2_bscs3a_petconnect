eb88511c818c5562988c7edf772628bd9280fae4
marktrex <sinicchi123@gmail.com>
2023-12-14 05:19:36 +0800

    added a dark function in the edit-profile page

--- dist/user/edit-profile.php
+++ dist/user/edit-profile.php
@@ -167,22 +167,7 @@ if (isset($_POST['delete'])) {
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <link rel="icon" href="../image/icon.png" type="image/png">
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pet Connect</title>
-    <link rel="stylesheet" href="..\css\newlyAdded\edit-profile-page.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Acme">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sigmar">
-    <script src="https://kit.fontawesome.com/98b545cfa6.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -199,6 +184,7 @@ if (isset($_POST['delete'])) {
     <script src="https://kit.fontawesome.com/98b545cfa6.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="..\css\colorStyle\user\edit-profile-color.css">
 </head>
 
 <body>
@@ -310,6 +296,8 @@ if (isset($_POST['delete'])) {
     </script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <?php require_once "..\components\call_across_pages.php"?>
+<?php require_once "..\components\light-switch.php"?>
+
 </body>
 
 </html>
